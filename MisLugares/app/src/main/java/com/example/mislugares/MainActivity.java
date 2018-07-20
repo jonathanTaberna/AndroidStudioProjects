8fd8ce5572bfa2a1f4c0ea26650b084f43b367e2
jonathan taberna <hellsing952@gmail.com>
2018-07-20 19:40:18 -0300

    20180720
    cambios realizados en la fecha 2018-07-20

--- MisLugares/app/src/main/java/com/example/mislugares/MainActivity.java
+++ MisLugares/app/src/main/java/com/example/mislugares/MainActivity.java
@@ -1,22 +1,29 @@
 package com.example.mislugares;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.VibrationEffect;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button bAcercaDe;
     private Button bSalir;
     private Button bPreferencias;
+    public static Lugares lugares = new LugaresVector();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,6 +93,10 @@ public class MainActivity extends AppCompatActivity {
             lanzarPreferencias(null);
             return true;
         }
+        if(id == R.id.menu_buscar) {
+            lanzarVistaLugar(null);
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 
@@ -100,6 +111,31 @@ public class MainActivity extends AppCompatActivity {
     public void lanzarPreferencias (View view) {
         Intent i = new Intent(this, PreferenciasActivity.class);
         startActivity(i);
+    }
+
+    public void lanzarVistaLugar(View view){
+        final EditText entrada = new EditText(this);
+        entrada.setText("0");
+        new AlertDialog.Builder(this)
+                .setTitle("Selección de lugar")
+                .setMessage("indica su id:")
+                .setView(entrada)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        long id = Long.parseLong(entrada.getText().toString());
+                        Intent i = new Intent(MainActivity.this, VistaLugarActivity.class);
+                        i.putExtra("id", id);
+                        startActivity(i);
+                    }})
+                .setNegativeButton("Cancelar", null)
+                .show();
+    }
+
+    public void mostrarPreferencias(View view){
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        String s = "notificaciones: "+ pref.getBoolean("notificaciones",true)
+                +", Máxima Distancia para notificar: " + pref.getString("maximo","?");
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
     }
 
 }
